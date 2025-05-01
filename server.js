d5974d4a65e509243467ba26cf846d54292cbe60
Bhumika <139799370+Bhumika0008@users.noreply.github.com>
2025-05-01 17:14:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotEnv = require('dotenv');
+const connectDb = require("./config/db");
+
+// dot env configuration
+dotEnv.config();
+
+// DataBase Connection
+connectDb()
+
+
+//rest object
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'));
+
+//route
+// URL: http://localhost:8000
+
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/restaurant", require("./routes/restaurantRoutes"));
+app.use("/api/v1/category", require("./routes/categoryRoutes"));
+app.use("/api/v1/foods", require("./routes/foodRoutes"));
+
+
+
+
+
+app.get("/", (req, res) => {
+    return res.status(200).send("<h1> Welcome to Food Server App</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+//listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`.bgWhite.red);
+});
